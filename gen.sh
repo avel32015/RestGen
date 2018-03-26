816c01d23cd461ddcc2271b3a3baa499fc15dff1
Velikotskiy Andrey Vladimirovich <ext.avelikotskiy@tcsbank.ru>
2018-03-26 13:05:17 +0500

    tunes

---
+++ gen.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+classpath=../lib/xalan.jar:../lib/serializer.jar
+schema=../smsgenerate.xsl
+
+if [ ! -f "$schema" ]; then
+    echo "ERROR: Schema not found"
+    exit 1
+fi
+
+rm -f -r java
+
+java_cmd="java -classpath $classpath org.apache.xalan.xslt.Process -in application.xml -xsl $schema"
+
+echo $java_cmd
+
+$java_cmd
